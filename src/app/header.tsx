5976c022229b9ca96fc05d10f5c24a78e11eedb9
hyun071111 <jisaki07@gmail.com>
2025-06-12 20:25:29 +0900

    feat: text change

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className='sticky top-0 z-10 bg-white/30 backdrop-blur-xl h-[70px] items-center flex px-4 border-b border-black/10'>
-      <Link href='/' className='text-2xl font-bold'>
-        MDX Blog
+      <Link href='/' className='text-3xl font-bold'>
+        hyun's blog
       </Link>
     </header>
   );
