f5707386d7455e6cc4c7f254c5f43395333e8faa
hyun071111 <jisaki07@gmail.com>
2025-06-19 14:47:12 +0900

    build: type error edit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 });
 
 module.exports = {
